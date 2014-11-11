bab9ff9d5f2538e1d4b42221acaaf9e06ed2943d
Javier García Escobedo <javier.garcia@ieslosremedios.org>
2014-11-11 10:28:37 +0100

    Añadido 2.5a

---
+++ E-2.5a Propiedades de tipos numéricos/PropiedadesNum.java
@@ -0,0 +1,41 @@
+public class PropiedadesNum {	
+    public static void main(String args[]) {    	
+		
+		// Datos del tipo Byte	
+		System.out.println("\nByte:");	
+		System.out.println("\tSize: " + Byte.SIZE);	
+		System.out.println("\tMin: " + Byte.MIN_VALUE);	
+		System.out.println("\tMax: " + Byte.MAX_VALUE);	
+			
+		// Datos del tipo Short	
+		System.out.println("\nShort:");	
+		System.out.println("\tSize: " + Short.SIZE);	
+		System.out.println("\tMin: " + Short.MIN_VALUE);	
+		System.out.println("\tMax: " + Short.MAX_VALUE);	
+	
+		// Datos del tipo Integer	
+		System.out.println("\nInteger:");	
+		System.out.println("\tSize: " + Integer.SIZE);	
+		System.out.println("\tMin: " + Integer.MIN_VALUE);	
+		System.out.println("\tMax: " + Integer.MAX_VALUE);	
+			
+		// Datos del tipo Long	
+		System.out.println("\nLong:");	
+		System.out.println("\tSize: " + Long.SIZE);	
+		System.out.println("\tMin: " + Long.MIN_VALUE);	
+		System.out.println("\tMax: " + Long.MAX_VALUE);	
+	
+		// Datos del tipo Float	
+		System.out.println("\nFloat:");	
+		System.out.println("\tSize: " + Float.SIZE);	
+		System.out.println("\tMin: " + Float.MIN_VALUE);	
+		System.out.println("\tMax: " + Float.MAX_VALUE);	
+	
+		// Datos del tipo Double	
+		System.out.println("\nInteger:");	
+		System.out.println("\tSize: " + Double.SIZE);	
+		System.out.println("\tMin: " + Double.MIN_VALUE);	
+		System.out.println("\tMax: " + Double.MAX_VALUE);	
+			
+	}	
+}	
